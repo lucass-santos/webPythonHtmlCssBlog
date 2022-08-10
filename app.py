09b55c8041af92c16a02c74f94e1446cf408453a
lucass-santos <lucas.ramuh@gmail.com>
2022-08-10 13:20:26 +0000

    Criada a rota contatos
    Nela foi adicionado código html.

--- app.py
+++ app.py
@@ -2,7 +2,20 @@ from flask import Flask
 
 app = Flask("hello")
 
-#
 @app.route("/")
 def hello():
-    return "Hello World"
+    return "Hello World!"
+
+
+@app.route("/contatos")
+def contato():
+    return """<html>
+                <head>
+                    <title> Contatos </title>
+                </head>
+                <body>
+                    <h1>Lucas Santos de Oliveira</h1>
+                    <h2>Rua tra la la</h2>
+                    <h3>Telefone: (11)98525-6341</h3>
+                </body>
+              </html>"""
